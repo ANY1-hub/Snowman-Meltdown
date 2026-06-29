158f6bd9ae56f4ac1091dd4264a57b900d1ee2b3
nyiry <dev@bejelentkezesek.mozmail.com>
2026-06-29 14:51:15 +0200

    While loop for UI

---
+++ snowman.py
@@ -0,0 +1,31 @@
+import random
+
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+def play_game():
+    secret_word = get_random_word()
+    print("Welcome to Snowman Meltdown!")
+    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    round_count = 1
+
+
+    while True:
+    # For now, simply prompt the user once:
+        guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+        if guess in WORDS:
+            print("Congrats! You saved the snowman!")
+            break
+        if round_count == 4:
+            print("ups, snowman dead.")
+            break
+        round_count += 1
+
+if __name__ == "__main__":
+    play_game()
+
