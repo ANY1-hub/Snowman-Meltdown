52c0f27470548ca2228387c95e7a1e2ad00267f1
nyiry <dev@bejelentkezesek.mozmail.com>
2026-06-29 17:49:24 +0200

    refactoring: get_random(), display_game_state(), play_game() to game_logic.py (module), play_game defines winning and loosing.

---
+++ game_logic.py
@@ -0,0 +1,50 @@
+import random
+from ascii_art import STAGES
+
+
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"
+         ]
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return random.choice(WORDS)
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    display_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
+def play_game():
+    secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+    print("Welcome to Snowman Meltdown!")
+    display_game_state(mistakes, secret_word, guessed_letters)
+
+
+
+    while True:
+        # For now, simply prompt the user once:
+        guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+        if guess not in secret_word:
+            mistakes += 1
+        if mistakes == 4:
+            print("ups, snowman dead.")
+            break
+        if guess in secret_word:
+            guessed_letters.append(guess)
+        display_game_state(mistakes, secret_word, guessed_letters)
+
+        if len(set(guessed_letters)) == len(set(secret_word)):
+            print('you saved the Snowman!')
+            break
